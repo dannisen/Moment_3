a3be69c386995c48f39029e17b6c8ca1dc68f651
dannisen <forsbergdaniela92@gmail.com>
2020-10-14 23:12:21 +0200

    -pub

--- pub/js/main.js
+++
@@ -1,1 +0,0 @@
-function closeNav(){document.getElementById("mobileMenu").style.width="0",document.getElementById("main").style.display="block"}function openNav(){document.getElementById("mobileMenu").style.width="100%",document.getElementById("main").style.display="none"}
